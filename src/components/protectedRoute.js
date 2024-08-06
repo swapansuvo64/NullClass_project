6f242c6a264aa8ad4cb114d7f1bf6c1a39df4e53
swapansuvo64 <127852432+swapansuvo64@users.noreply.github.com>
2024-08-07 03:03:18 +0530

    Update protectedRoute.js

--- src/components/protectedRoute.js
+++ src/components/protectedRoute.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { useAuthState } from 'react-firebase-hooks/auth'; // Use useAuthState
-import { auth } from '../firebase.init'; // Adjust the path as needed
+import { useAuthState } from 'react-firebase-hooks/auth'; 
+import { auth } from '../firebase.init';
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
     const [user, loading, error] = useAuthState(auth);
 
     if (loading) {
-        return <div>Loading...</div>; // You can customize this as needed
+        return <div>Loading...</div>;
     }
 
     if (error) {
         console.error(error);
-        return <div>Error: {error.message}</div>; // Customize error handling
+        return <div>Error: {error.message}</div>; 
     }
 
     if (!user) {
